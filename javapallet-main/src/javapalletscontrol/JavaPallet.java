8c839410e5401b9d4fc35091861b9392839daae4
Esaú Paiva <101275215+esaupdn@users.noreply.github.com>
2023-03-16 10:15:48 -0300

    Add files via upload

---
+++ javapallet-main/src/javapalletscontrol/JavaPallet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapalletscontrol;
+
+/**
+ *
+ * @author mefma
+ */
+public class JavaPallet {
+         
+    public static void main(String[] args) {
+        FormSistema f = new FormSistema();
+        f.setVisible(true);
+    }
+    
+    
+    
+}
